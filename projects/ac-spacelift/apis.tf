3b79941635c5aeb1af3cf3ff1d583d6d2bb37113
atom <adamenger@gmail.com>
2023-06-01 17:11:22 -0500

    updating apis

--- projects/ac-spacelift/apis.tf
+++ projects/ac-spacelift/apis.tf
@@ -1,6 +1,17 @@
-resource "google_project_service" "compute" {
+locals {
+  apis = [
+    "compute.googleapis.com",
+    "container.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "cloudbilling.googleapis.com",
+    "serviceusage.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "apis" {
+  for_each = toset(local.apis)
   project = google_project.ac-spacelift.project_id
-  service = "compute.googleapis.com"
+  service = each.key
 
   timeouts {
     create = "30m"
@@ -10,38 +21,3 @@ resource "google_project_service" "compute" {
   disable_dependent_services = true
 }
 
-resource "google_project_service" "container" {
-  project = google_project.ac-spacelift.project_id
-  service = "container.googleapis.com"
-
-  timeouts {
-    create = "30m"
-    update = "40m"
-  }
-
-  disable_dependent_services = true
-}
-
-resource "google_project_service" "cloudresourcemanager" {
-  project = google_project.ac-spacelift.project_id
-  service = "cloudresourcemanager.googleapis.com"
-
-  timeouts {
-    create = "30m"
-    update = "40m"
-  }
-
-  disable_dependent_services = true
-}
-
-resource "google_project_service" "billing" {
-  project = google_project.ac-spacelift.project_id
-  service = "cloudbilling.googleapis.com"
-
-  timeouts {
-    create = "30m"
-    update = "40m"
-  }
-
-  disable_dependent_services = true
-}

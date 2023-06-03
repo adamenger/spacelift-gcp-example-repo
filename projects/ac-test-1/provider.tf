e1c1a34995d9a7629e9d243686ad42a5e3ea6004
atom <adamenger@gmail.com>
2023-06-02 21:29:04 -0500

    changing impersonated account

--- projects/ac-test-1/provider.tf
+++ projects/ac-test-1/provider.tf
@@ -1,8 +1,8 @@
 provider "google" {
   project     = "atomic-computer-test-1"
   region      = "us-central1"
-  impersonate_service_account = "spacelift-project-creator@ac-spacelift.iam.gserviceaccount.com"
+  #impersonate_service_account = "spacelift-project-creator@ac-spacelift.iam.gserviceaccount.com"
+  impersonate_service_account = "spacelift-operator@atomic-computer-test-1.iam.gserviceaccount.com"
 }
 
 
-#impersonate_service_account = "spacelift-operator@atomic-computer-test-1.iam.gserviceaccount.com"

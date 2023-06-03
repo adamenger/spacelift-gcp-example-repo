338f55990e36720b64565bbf93d54260db6766fb
atom <adamenger@gmail.com>
2023-06-02 22:00:52 -0500

    updating comments on iam bindings

--- organization/iam.tf
+++ organization/iam.tf
@@ -2,6 +2,7 @@ data "google_organization" "org" {
   organization = "76970739165"
 }
 
+# allowing myself access to the org
 resource "google_organization_iam_binding" "owner" {
   role     = "roles/owner"
   org_id   = data.google_organization.org.org_id
@@ -31,7 +32,7 @@ resource "google_organization_iam_binding" "spacelift-project-iam" {
   ]
 }
 
-# these bindings allow spacelift-project-creator service account to add iam bindings to projects
+# these bindings allow spacelift-project-creator service account to administer service accounts on the project
 resource "google_organization_iam_binding" "spacelift-sa-creator" {
   role     = "roles/iam.serviceAccountAdmin"
   org_id   = data.google_organization.org.org_id
@@ -41,7 +42,7 @@ resource "google_organization_iam_binding" "spacelift-sa-creator" {
   ]
 }
 
-# these bindings allow spacelift-project-creator service account to add iam bindings to projects
+# these bindings allow spacelift-project-creator service account to add billing details to new projects
 resource "google_organization_iam_binding" "spacelift-billing-user" {
   role     = "roles/billing.user"
   org_id   = data.google_organization.org.org_id
@@ -51,6 +52,7 @@ resource "google_organization_iam_binding" "spacelift-billing-user" {
   ]
 }
 
+# these bindings allow spacelift-project-creator service account to enable apis on the newly created project
 resource "google_organization_iam_binding" "spacelift-api-admin" {
   role     = "roles/serviceusage.serviceUsageAdmin"
   org_id   = data.google_organization.org.org_id

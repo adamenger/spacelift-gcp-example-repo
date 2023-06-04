af8cd92a2353d03151f21205411caced55322169
atom <adamenger@gmail.com>
2023-06-04 16:28:41 -0500

    adding missing ,

--- projects/ac-test-3/spacelift.tf
+++ projects/ac-test-3/spacelift.tf
@@ -1,7 +1,7 @@
 module "spacelift-operator" {
   source = "../../modules/spacelift-operator"
   spacelift_worker_service_account = "spacelift@ac-spacelift.iam.gserviceaccount.com"
-  roles  = [
-    "pubsub.admin"
+  roles = [
+    "pubsub.admin",
   ]
 }

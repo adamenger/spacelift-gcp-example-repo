f6701a27de20bf1a5b6d2d58e4668a78f8eae1ca
atom <adamenger@gmail.com>
2023-06-04 16:13:34 -0500

    adding statebucket for ac-test-3

--- projects/ac-spacelift/buckets.tf
+++ projects/ac-spacelift/buckets.tf
@@ -45,3 +45,27 @@ data "google_iam_policy" "ac-test-2-state-admin" {
     ]
   }
 }
+
+resource "google_storage_bucket" "ac-test-3-state" {
+  name          = "ac-test-3-spacelift-state"
+  project       = google_project.project.project_id
+  location      = "US"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_iam_policy" "ac-test-3-state-policy" {
+  bucket        = google_storage_bucket.ac-test-3-state.name
+  policy_data   = data.google_iam_policy.ac-test-3-state-admin.policy_data
+}
+
+data "google_iam_policy" "ac-test-3-state-admin" {
+  binding {
+    role = "roles/storage.admin"
+    members = [
+      "user:admin-adam@atomic.computer",
+      "serviceAccount:${google_service_account.spacelift.email}",
+    ]
+  }
+}

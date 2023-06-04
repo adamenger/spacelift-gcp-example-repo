3350e9b7ed0e451f338dbbb6e14df17077800625
atom <adamenger@gmail.com>
2023-06-04 16:29:22 -0500

    operator_roles, not roles

--- projects/ac-test-3/spacelift.tf
+++ projects/ac-test-3/spacelift.tf
@@ -1,7 +1,7 @@
 module "spacelift-operator" {
   source = "../../modules/spacelift-operator"
   spacelift_worker_service_account = "spacelift@ac-spacelift.iam.gserviceaccount.com"
-  roles = [
+  operator_roles = [
     "pubsub.admin",
   ]
 }

4883e5052ae4cef04ac320fd337cb0edaa027b3c
atom <adamenger@gmail.com>
2023-06-05 20:18:18 -0500

    trying out the name space from github enterprise block

--- spacelift/stacks.tf
+++ spacelift/stacks.tf
@@ -5,6 +5,9 @@ locals {
 resource "spacelift_stack" "managed-stacks" {
   for_each          = toset(local.stacks.stacks)
   terraform_version = "1.4.6"
+  github_enterprise {
+    namespace       = "adamenger"
+  }
 
   name              = "GCP: ${each.value}"
   repository        = "spacelift-gcp-example-repo"

b5e528a7bcc29b69313ab3e37e530b41d57d6beb
atom <adamenger@gmail.com>
2023-06-05 19:39:03 -0500

    updating spacelift folder

--- spacelift/provider.tf
+++ spacelift/provider.tf
@@ -1,8 +1,9 @@
-variable "spacelift_key_id" {}
-variable "spacelift_key_secret" {}
-
-provider "spacelift" {
-  api_key_endpoint = "https://wtfenterprises.app.spacelift.io"
-  api_key_id       = var.spacelift_key_id
-  api_key_secret   = var.spacelift_key_secret
+terraform {
+  required_providers {
+    spacelift = {
+      source = "spacelift-io/spacelift"
+    }
+  }
 }
+
+provider "spacelift" {}

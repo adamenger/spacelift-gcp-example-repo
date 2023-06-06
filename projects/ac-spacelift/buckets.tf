66b8ba53f1d71be214d55598381d27c4c9f812c6
atom <adamenger@gmail.com>
2023-06-05 20:09:45 -0500

    making bucket creation hinged on stacks.yml in root

--- projects/ac-spacelift/buckets.tf
+++ projects/ac-spacelift/buckets.tf
@@ -1,5 +1,10 @@
-resource "google_storage_bucket" "ac-test-1-state" {
-  name          = "ac-test-1-spacelift-state"
+locals {
+ stacks = yamldecode(file("../../stacks.yml"))
+}
+
+resource "google_storage_bucket" "state-buckets" {
+  for_each      = toset(local.stacks.stacks)
+  name          = "${each.value}-spacelift"
   project       = google_project.project.project_id
   location      = "US"
   force_destroy = true
@@ -7,60 +12,13 @@ resource "google_storage_bucket" "ac-test-1-state" {
   uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket_iam_policy" "ac-test-1-state-policy" {
-  bucket        = google_storage_bucket.ac-test-1-state.name
-  policy_data   = data.google_iam_policy.ac-test-1-state-admin.policy_data
+resource "google_storage_bucket_iam_policy" "state-policy" {
+  for_each      = toset(local.stacks.stacks)
+  bucket        = google_storage_bucket.state-buckets.name
+  policy_data   = data.google_iam_policy.state-admin.policy_data
 }
 
-data "google_iam_policy" "ac-test-1-state-admin" {
-  binding {
-    role = "roles/storage.admin"
-    members = [
-      "user:admin-adam@atomic.computer",
-      "serviceAccount:${google_service_account.spacelift.email}",
-    ]
-  }
-}
-
-resource "google_storage_bucket" "ac-test-2-state" {
-  name          = "ac-test-2-spacelift-state"
-  project       = google_project.project.project_id
-  location      = "US"
-  force_destroy = true
-
-  uniform_bucket_level_access = true
-}
-
-resource "google_storage_bucket_iam_policy" "ac-test-2-state-policy" {
-  bucket        = google_storage_bucket.ac-test-2-state.name
-  policy_data   = data.google_iam_policy.ac-test-2-state-admin.policy_data
-}
-
-data "google_iam_policy" "ac-test-2-state-admin" {
-  binding {
-    role = "roles/storage.admin"
-    members = [
-      "user:admin-adam@atomic.computer",
-      "serviceAccount:${google_service_account.spacelift.email}",
-    ]
-  }
-}
-
-resource "google_storage_bucket" "ac-test-3-state" {
-  name          = "ac-test-3-spacelift-state"
-  project       = google_project.project.project_id
-  location      = "US"
-  force_destroy = true
-
-  uniform_bucket_level_access = true
-}
-
-resource "google_storage_bucket_iam_policy" "ac-test-3-state-policy" {
-  bucket        = google_storage_bucket.ac-test-3-state.name
-  policy_data   = data.google_iam_policy.ac-test-3-state-admin.policy_data
-}
-
-data "google_iam_policy" "ac-test-3-state-admin" {
+data "google_iam_policy" "state-admin" {
   binding {
     role = "roles/storage.admin"
     members = [
